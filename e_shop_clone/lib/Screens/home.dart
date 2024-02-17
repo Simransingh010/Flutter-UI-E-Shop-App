b5440185ac425f96853bd7e168e922414e1a5a7f
Simransingh010 <simran.ahuja0010@gmail.com>
2024-02-17 11:52:28 +0530

    commit 2

--- e_shop_clone/lib/Screens/home.dart
+++ e_shop_clone/lib/Screens/home.dart
@@ -1,5 +1,3 @@
-import 'dart:html';
-
 import 'package:e_shop_clone/Screens/Drawer.dart';
 import 'package:flutter/material.dart';
 
@@ -33,11 +31,100 @@ class Home extends StatelessWidget {
         ],
       ),
       drawer: const DrawerCall(),
-      body: Center(
-        child: Text(
-          'Simran',
-          style: TextStyle(fontSize: 40, color: Colors.black),
-        ),
+      body: Column(
+        children: [
+          Container(
+            width: 500,
+            padding: EdgeInsets.only(top: 0, bottom: 25, left: 0, right: 0),
+            margin: EdgeInsets.only(
+              top: 25,
+              right: 45,
+              left: 50,
+            ),
+            decoration: BoxDecoration(
+              color: Colors.purple,
+              borderRadius: BorderRadius.circular(12.0),
+            ),
+            child: IntrinsicHeight(
+              child: Row(
+                children: [
+                  SizedBox(
+                    height: 60,
+                    width: 150,
+                    child: Padding(
+                      padding: const EdgeInsets.only(
+                        right: 0,
+                        left: 0,
+                        top: 10,
+                      ),
+                      child: ListTile(
+                        leading: Icon(
+                          Icons.wallet_rounded,
+                          color: Colors.white,
+                        ),
+                        title: Text(
+                          'My Wallet',
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: Colors.white,
+                            fontWeight: FontWeight.w900,
+                          ),
+                        ),
+                        subtitle: Text(
+                          'Rs 30,432',
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 12,
+                              decoration: TextDecoration.underline,
+                              decorationColor: Colors.white,
+                              decorationThickness: 1,
+                              fontWeight: FontWeight.w500),
+                        ),
+                      ),
+                    ),
+                  ),
+                  VerticalDivider(
+                    color: Colors.white,
+                    thickness: 2.0,
+                    indent: 20,
+                  ),
+                  SizedBox(
+                    height: 60,
+                    width: 160,
+                    child: Padding(
+                      padding: const EdgeInsets.only(
+                          left: 0, right: 0, top: 10, bottom: 10),
+                      child: ListTile(
+                        leading: Icon(
+                          Icons.money,
+                          color: Colors.white,
+                        ),
+                        title: Text(
+                          'My Coins',
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: Colors.white,
+                            fontWeight: FontWeight.w900,
+                          ),
+                        ),
+                        subtitle: Text(
+                          'Rs 30000',
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 12,
+                              decoration: TextDecoration.underline,
+                              decorationColor: Colors.white,
+                              decorationThickness: 2,
+                              fontWeight: FontWeight.w500),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
